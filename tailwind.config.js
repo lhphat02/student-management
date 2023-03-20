50bd8ee7212ae09da437b33c688d65b2aabd7cf3
phatluu <lhphat.02@gmail.com>
2023-03-20 20:45:02 +0700

    Initial config

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/flowbite/**/*.js',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('flowbite/plugin')],
+};
