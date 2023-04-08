d3ab04d2217d926a89df72e3c103d0da688b39f1
phatluu <lhphat.02@gmail.com>
2023-04-08 16:28:26 +0700

    Update

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,159 +1,135 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import router, {useRouter} from 'next/router';
+import router, { useRouter } from 'next/router';
 
 import assets from '../assets';
 
 const Sidebar = ({ children }) => {
-  return (
-    <div className='flex'>
-      <div className='fixed w-20 h-screen p-4 bg-slate-300 border-r-[1px] flex flex-col justify-between'>
-        <div className='flex flex-col items-center'>
+  const routing = useRouter();
 
-        <Link href='/'>
-            <div className='bg-white text-white p-3 rounded-lg inline-block '>
-            <Image 
+  return (
+    <div className={`flex ${routing.pathname === '/rules' ? 'hidden' : null}`}>
+      <div className="fixed w-20 h-screen p-4 bg-slate-300 border-r-[1px] flex flex-col justify-between">
+        <div className="flex flex-col items-center">
+          <Link href="/">
+            <div className="inline-block p-3 text-white bg-white rounded-lg ">
+              <Image
                 src={assets.vocational}
-                alt='logo'
+                alt="logo"
                 width={40}
                 height={40}
               />
             </div>
           </Link>
 
-          <span className='border-b-[1px] border-white w-full p-2'></span>
+          <span className="border-b-[1px] border-white w-full p-2"></span>
 
-          <Link href='/'>
-            <div className=' hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
-              <div className='flex flex-row'>
-              <Image 
-                src={assets.dashboard}
-                alt='dashboard'
-                width={25}
-                height={25}
-              />
-              {/* <p className='text-md font-poppins mt-1 ml-3'>Dashboard</p> */}
+          <Link href="/">
+            <div className="inline-block p-3 my-4 rounded-lg cursor-pointer hover:bg-gray-200">
+              <div className="flex flex-row">
+                <Image
+                  src={assets.dashboard}
+                  alt="dashboard"
+                  width={25}
+                  height={25}
+                />
+                {/* <p className='mt-1 ml-3 text-md font-poppins'>Dashboard</p> */}
               </div>
             </div>
           </Link>
 
-          <Link href='/teachermanage'>
-            <div className=' hover:bg-gray-200 cursor-pointer my-3 p-3 rounded-lg inline-block'>
-              <div className='flex flex-row'>
-               <Image 
-                src={assets.teacher}
-                alt='teacher'
-                width={25}
-                height={25}
+          <Link href="/teachermanage">
+            <div className="inline-block p-3 my-3 rounded-lg cursor-pointer hover:bg-gray-200">
+              <div className="flex flex-row">
+                <Image
+                  src={assets.teacher}
+                  alt="teacher"
+                  width={25}
+                  height={25}
                 />
-              {/* <p className='text-md font-poppins mt-1 ml-3'>Teacher</p> */}
+                {/* <p className='mt-1 ml-3 text-md font-poppins'>Teacher</p> */}
               </div>
             </div>
           </Link>
 
-          <Link href='/studentmanage'>
-          <div className=' hover:bg-gray-200 cursor-pointer my-3 p-3 rounded-lg inline-block'>
-              <div className='flex flex-row'>
-               <Image 
-                src={assets.cap}
-                alt='student'
-                width={25}
-                height={25}
-                />
-              {/* <p className='text-md font-poppins mt-1 ml-3'>Student</p> */}
+          <Link href="/studentmanage">
+            <div className="inline-block p-3 my-3 rounded-lg cursor-pointer hover:bg-gray-200">
+              <div className="flex flex-row">
+                <Image src={assets.cap} alt="student" width={25} height={25} />
+                {/* <p className='mt-1 ml-3 text-md font-poppins'>Student</p> */}
               </div>
             </div>
           </Link>
 
-          <Link href='/subject'>
-          <div className=' hover:bg-gray-200 cursor-pointer my-3 p-3 rounded-lg inline-block'>
-              <div className='flex flex-row'>
-               <Image 
-                src={assets.bookshelf}
-                alt='subject'
-                width={25}
-                height={25}
+          <Link href="/subject">
+            <div className="inline-block p-3 my-3 rounded-lg cursor-pointer hover:bg-gray-200">
+              <div className="flex flex-row">
+                <Image
+                  src={assets.bookshelf}
+                  alt="subject"
+                  width={25}
+                  height={25}
                 />
-              {/* <p className='text-md font-poppins mt-1 ml-3'>Subject</p> */}
+                {/* <p className='mt-1 ml-3 text-md font-poppins'>Subject</p> */}
               </div>
             </div>
           </Link>
 
-          <Link href='/class'>
-          <div className=' hover:bg-gray-200 cursor-pointer my-3 p-3 rounded-lg inline-block'>
-              <div className='flex flex-row'>
-               <Image 
-                src={assets.training}
-                alt='class'
-                width={25}
-                height={25}
+          <Link href="/class">
+            <div className="inline-block p-3 my-3 rounded-lg cursor-pointer hover:bg-gray-200">
+              <div className="flex flex-row">
+                <Image
+                  src={assets.training}
+                  alt="class"
+                  width={25}
+                  height={25}
                 />
-              {/* <p className='text-md font-poppins mt-1 ml-3'>Class</p> */}
+                {/* <p className='mt-1 ml-3 text-md font-poppins'>Class</p> */}
               </div>
             </div>
           </Link>
 
-          <Link href='/report'>
-          <div className=' hover:bg-gray-200 cursor-pointer my-3 p-3 rounded-lg inline-block'>
-              <div className='flex flex-row'>
-               <Image 
-                src={assets.file}
-                alt='report'
-                width={25}
-                height={25}
-                />
-              {/* <p className='text-md font-poppins mt-1 ml-3'>Report</p> */}
+          <Link href="/report">
+            <div className="inline-block p-3 my-3 rounded-lg cursor-pointer hover:bg-gray-200">
+              <div className="flex flex-row">
+                <Image src={assets.file} alt="report" width={25} height={25} />
+                {/* <p className='mt-1 ml-3 text-md font-poppins'>Report</p> */}
               </div>
             </div>
           </Link>
 
-          <Link href='/grade'>
-          <div className=' hover:bg-gray-200 cursor-pointer my-3 p-3 rounded-lg inline-block'>
-              <div className='flex flex-row'>
-               <Image 
-                src={assets.board}
-                alt='grade'
-                width={25}
-                height={25}
-                />
-              {/* <p className='text-md font-poppins mt-1 ml-3'>Grade</p> */}
+          <Link href="/grade">
+            <div className="inline-block p-3 my-3 rounded-lg cursor-pointer hover:bg-gray-200">
+              <div className="flex flex-row">
+                <Image src={assets.board} alt="grade" width={25} height={25} />
+                {/* <p className='mt-1 ml-3 text-md font-poppins'>Grade</p> */}
               </div>
             </div>
           </Link>
 
-          <Link href='/rules'>
-          <div className=' hover:bg-gray-200 cursor-pointer my-3 p-3 rounded-lg inline-block'>
-              <div className='flex flex-row'>
-               <Image 
-                src={assets.scales}
-                alt='rule'
-                width={25}
-                height={25}
-                />
-              {/* <p className='text-md font-poppins mt-1 ml-3'>Rule</p> */}
+          <Link href="/rules">
+            <div className="inline-block p-3 my-3 rounded-lg cursor-pointer hover:bg-gray-200">
+              <div className="flex flex-row">
+                <Image src={assets.scales} alt="rule" width={25} height={25} />
+                {/* <p className='mt-1 ml-3 text-md font-poppins'>Rule</p> */}
               </div>
             </div>
           </Link>
 
-          <span className='border-b-[1px] border-white w-full p-2'></span>
+          <span className="border-b-[1px] border-white w-full p-2"></span>
 
-          <Link href='/rules'>
-          <div className=' hover:bg-gray-200 cursor-pointer my-3 p-3 rounded-lg inline-block'>
-              <div className='flex flex-row'>
-               <Image 
-                src={assets.login}
-                alt='rule'
-                width={25}
-                height={25}
-                />
-              {/* <p className='text-md font-poppins mt-1 ml-3'>Rule</p> */}
+          <Link href="/rules">
+            <div className="inline-block p-3 my-3 rounded-lg cursor-pointer hover:bg-gray-200">
+              <div className="flex flex-row">
+                <Image src={assets.login} alt="rule" width={25} height={25} />
+                {/* <p className='mt-1 ml-3 text-md font-poppins'>Rule</p> */}
               </div>
             </div>
           </Link>
         </div>
       </div>
-      <main className='ml-20 w-full'>{children}</main>
+      <main className="w-full ml-20">{children}</main>
     </div>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -7,19 +7,16 @@ import Topbar from '../components/Topbar';
 export default function App({ Component, pageProps }) {
   return (
     <>
-    
-    <Head>
-    <title>Quản lý học sinh</title>
-    <meta name="description" content="Trang web quản lý học sinh" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="/favicon.ico" />
-  </Head>
-  
-    <Sidebar>
-      <Component {...pageProps} />
-    </Sidebar>
-  
-    </>
+      <Head>
+        <title>Quản lý học sinh</title>
+        <meta name="description" content="Trang web quản lý học sinh" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
+      <Sidebar>
+        <Component {...pageProps} />
+      </Sidebar>
+    </>
   );
 }
