0d11817ca5a744690feb1712d04d5c7abee1150f
Minhquan257 <denonxskt@gmail.com>
2023-04-10 13:12:42 +0700

    update table for pages

---
+++ components/Table.jsx
@@ -0,0 +1,24 @@
+import { Table } from "flowbite-react";
+
+const MyTable = ({ nametable, infor,  header, body }) => {
+    // return(
+    //     <div className="w-full">
+    //     <p className=" p-5 text-3xl font-poppins font-bold">{nametable}</p>
+
+    //     <div className="w-8/9">
+    //     <Table>
+    //         {/* <Table.Head> */}
+    //             {/* <Table.HeadCell className="flex justify-center items-center">{nametable}</Table.HeadCell> */}
+    //             {header}
+    //         {/* </Table.Head> */}
+    //         {/* <Table.Body className="divide-y"> */}
+    //             {body}
+    //         {/* </Table.Body> */}
+    //     </Table>
+    //     </div>
+    //     </div>
+    // )
+}
+
+
+export default MyTable;
--- pages/studentmanage.js
+++ pages/studentmanage.js
@@ -1,5 +1,8 @@
 import React from "react";
 // import { ThemeProvider } from 'next-themes';
+import { Table } from "flowbite-react";
+
+import MyTable from "../components/Table";
 
 
 import Topbar from "../components/Topbar";
@@ -8,6 +11,126 @@ const StudentManagement = () => {
     return(
         <div>
             <Topbar NamePage='Student Management'/>
+            <div>
+            <p className=" p-5 text-3xl font-poppins font-bold">Danh sách học sinh</p>
+            {/* <MyTable */}
+            <Table>
+                {/* nametable="Danh sách học sinh" 
+                header={ */}
+                    <Table.Head>
+                        <Table.HeadCell className="whitespace-nowrap font-medium text-gray-900">
+                            STT 
+                        </Table.HeadCell>
+                        <Table.HeadCell className="whitespace-nowrap font-medium  text-gray-900">
+                           Họ Tên
+                        </Table.HeadCell>
+                        <Table.HeadCell className="whitespace-nowrap font-medium  text-gray-900">
+                            Lớp 
+                        </Table.HeadCell>
+                        <Table.HeadCell className="whitespace-nowrap font-medium  text-gray-900">
+                            TB Học Kỳ I 
+                        </Table.HeadCell>
+                        <Table.HeadCell className="whitespace-nowrap font-medium text-gray-900">
+                            TB Học Kỳ II 
+                        </Table.HeadCell>
+                    </Table.Head>
+                {/* } */}
+
+                {/* body={ */}
+                    <Table.Body className=" divide-y">    
+                    <Table.Row className="bg-white">
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y text-gray-900">
+                            1 
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y  text-gray-900">
+                            Nguyễn Minh Quân
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y  text-gray-900">
+                             10A4
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y  text-gray-900">
+                              9
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y text-gray-900">
+                             7
+                        </Table.Cell>
+                    </Table.Row>
+                    <Table.Row className="bg-white">
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y text-gray-900">
+                            2 
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y  text-gray-900">
+                            
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y  text-gray-900">
+                             
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y  text-gray-900">
+                              
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y text-gray-900">
+                             
+                        </Table.Cell>
+                    </Table.Row>
+
+                    <Table.Row className="bg-white">
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y text-gray-900">
+                            3 
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y  text-gray-900">
+                            
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y  text-gray-900">
+                             
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y  text-gray-900">
+                              
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y text-gray-900">
+                             
+                        </Table.Cell>
+                    </Table.Row>
+
+                    <Table.Row className="bg-white">
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y text-gray-900">
+                            4 
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y  text-gray-900">
+                            
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y  text-gray-900">
+                             
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y  text-gray-900">
+                              
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y text-gray-900">
+                             
+                        </Table.Cell>
+                    </Table.Row>
+
+                    <Table.Row className="bg-white">
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y text-gray-900">
+                            5 
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y  text-gray-900">
+                            
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y  text-gray-900">
+                             
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y  text-gray-900">
+                              
+                        </Table.Cell>
+                        <Table.Cell className="whitespace-nowrap font-medium  border-y text-gray-900">
+                             
+                        </Table.Cell>
+                    </Table.Row>
+                </Table.Body>
+                {/* } */}
+            {/* /> */}
+            </Table>
+            </div>
         </div>
     )
 }
