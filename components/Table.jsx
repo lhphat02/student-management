fd547b895532171e630bd92a98510842ca8af68e
phatluu <lhphat.02@gmail.com>
2023-04-14 21:58:23 +0700

    Update

--- components/Table.jsx
+++ components/Table.jsx
@@ -1,36 +1,54 @@
-import { Table } from "flowbite-react";
+import React from 'react';
+import classNames from 'classnames';
 
-const MyTable = ({ cloumn1, column2, column3, column4, data }) => {
+const Table = ({ tableInstance }) => {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
 
-  
   return (
-    <div className="w-full flex items-center justify-center">
-      <div className="w-4/5">
-        <Table>
-
-          <Table.Head>
-            <Table.HeadCell className="table-style">STT</Table.HeadCell>
-            <Table.HeadCell className="table-style">{cloumn1}</Table.HeadCell>
-            <Table.HeadCell className="table-style">{column2}</Table.HeadCell>
-            <Table.HeadCell className="table-style">{column3}</Table.HeadCell>
-            <Table.HeadCell className="table-style">{column4}</Table.HeadCell>
-          </Table.Head>
-          
-          <Table.Body className=" divide-y">
-            {students.map((data) => (
-              <Table.Row key={data.id} className="bg-white">
-                <Table.Cell className="table-style">{data.col1}</Table.Cell>
-                <Table.Cell className="table-style">{data.col2}</Table.Cell>
-                <Table.Cell className="table-style">{data.col3}</Table.Cell>
-                <Table.Cell className="table-style">{data.col4}</Table.Cell>
-                <Table.Cell className="table-style">{data.col5}</Table.Cell>
-              </Table.Row>
+    <table
+      {...getTableProps()}
+      className="relative w-full bg-white rounded-lg border"
+    >
+      <thead className=" border-2">
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th
+                {...column.getHeaderProps()}
+                className={classNames(
+                  'px-4 py-2 text-left text-sm font-medium text-gray-600 uppercase tracking-wider',
+                  column.className
+                )}
+              >
+                {column.render('Header')}
+              </th>
             ))}
-          </Table.Body>
-        </Table>
-      </div>
-    </div>
+          </tr>
+        ))}
+      </thead>
+      <tbody className="" {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()} className="border-t border-gray-200">
+              {row.cells.map((cell) => (
+                <td
+                  {...cell.getCellProps()}
+                  className={classNames(
+                    'px-4 py-2 text-sm font-normal text-gray-900',
+                    cell.column.className
+                  )}
+                >
+                  {cell.render('Cell')}
+                </td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
-export default MyTable;
+export default Table;
